da5781ee5d51a98c897e7e5568e9e87da3d9c27f
GuillermoVazquez <wvvazque@gmail.com>
2018-03-10 18:37:50 -0500

    mapchat update

--- MapChat/app/src/main/java/vazquez/guillermo/mapchat/Fragments/MapFragment.java
+++ MapChat/app/src/main/java/vazquez/guillermo/mapchat/Fragments/MapFragment.java
@@ -1,11 +1,23 @@
 package vazquez.guillermo.mapchat.Fragments;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import vazquez.guillermo.mapchat.R;
 
@@ -13,6 +25,9 @@ import vazquez.guillermo.mapchat.R;
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
+
+    private GoogleMap googleMap;
+    MapView mapView;
     View v;
 
     public MapFragment() {
@@ -31,10 +46,65 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_map, container, false);
-        //todo: generate google map
-        //todo: retrieve nearby users to display on map
+        //generate MapView
+        mapView =  v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        //to display the map immediately
+        mapView.onResume();
 
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap gMap) {
+                googleMap = gMap;
+
+                //check for permissions
+                if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                }
+                else googleMap.setMyLocationEnabled(true);  // For showing a move to my location button
+
+                //todo: access server and get latlng of nearby users to place markers on map
+                //todo: below code for sydney
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                //camera
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+        });
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
