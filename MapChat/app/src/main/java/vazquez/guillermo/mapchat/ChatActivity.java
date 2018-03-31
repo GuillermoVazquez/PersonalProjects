93277e619c5448a9823fd6d664e277b953c0e325
GuillermoVazquez <wvvazque@gmail.com>
2018-03-31 02:41:32 -0400

    message ui

---
+++ MapChat/app/src/main/java/vazquez/guillermo/mapchat/ChatActivity.java
@@ -0,0 +1,39 @@
+package vazquez.guillermo.mapchat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import vazquez.guillermo.mapchat.MessageStuff.MessageListAdapter;
+
+public class ChatActivity extends AppCompatActivity {
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+}
