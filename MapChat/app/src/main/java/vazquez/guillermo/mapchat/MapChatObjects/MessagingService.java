ff5042561c3fa64d965baae6db45654877d98848
GuillermoVazquez <wvvazque@gmail.com>
2018-04-05 13:49:07 -0400

    message encryption/decryption

--- MapChat/app/src/main/java/vazquez/guillermo/mapchat/MapChatObjects/MessagingService.java
+++ MapChat/app/src/main/java/vazquez/guillermo/mapchat/MapChatObjects/MessagingService.java
@@ -12,12 +12,22 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+
+import vazquez.guillermo.mapchat.KeyStuff.Converters;
+import vazquez.guillermo.mapchat.KeyStuff.Crypto;
+
 import static com.android.volley.VolleyLog.TAG;
 
 //to receive payloads
 //to send upstream messages
 //to receive notifications
 public class MessagingService extends FirebaseMessagingService {
+    SharedPreferences sharedPreferences = this.getSharedPreferences("partners",MODE_PRIVATE);
+    Crypto crypto = new Crypto();
+    Converters converters = new Converters();
 
     //constructor
     public MessagingService() {
@@ -35,11 +45,22 @@ public class MessagingService extends FirebaseMessagingService {
                 JSONObject jsonObject = new JSONObject(jsonString);
                 String partner = jsonObject.getString("from");
                 //encrypted message received as string here in PEM format
-                String message = jsonObject.getString("message");
-
+                String partnerMessage = jsonObject.getString("message");
+                //get partner puclic key
+                //publickey as String
+                String publicKeyString = sharedPreferences.getString(partner,"");
+                //convert public key String to publicKey
+                PublicKey publicKey = converters.convertPublic(publicKeyString);
+                //decrypt the partner message
+                String message = crypto.
+                String message
                 //now send to stored messages
-                
+
             } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (InvalidKeySpecException e) {
                 e.printStackTrace();
             }
 
