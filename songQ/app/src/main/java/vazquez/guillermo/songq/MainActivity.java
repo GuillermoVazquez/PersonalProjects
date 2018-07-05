4d88e7fc6a4556c356b967c917044d34dc17eeb1
GuillermoVazquez <wvvazque@gmail.com>
2018-07-04 20:50:06 -0400

    auth process work

--- songQ/app/src/main/java/vazquez/guillermo/songq/MainActivity.java
+++ songQ/app/src/main/java/vazquez/guillermo/songq/MainActivity.java
@@ -1,20 +1,21 @@
 package vazquez.guillermo.songq;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.util.Log;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
-        import com.spotify.sdk.android.authentication.AuthenticationClient;
-        import com.spotify.sdk.android.authentication.AuthenticationRequest;
-        import com.spotify.sdk.android.authentication.AuthenticationResponse;
-        import com.spotify.sdk.android.player.Config;
-        import com.spotify.sdk.android.player.ConnectionStateCallback;
-        import com.spotify.sdk.android.player.Error;
-        import com.spotify.sdk.android.player.Player;
-        import com.spotify.sdk.android.player.PlayerEvent;
-        import com.spotify.sdk.android.player.Spotify;
-        import com.spotify.sdk.android.player.SpotifyPlayer;
+import static com.spotify.sdk.android.authentication.LoginActivity.REQUEST_CODE;
 
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
@@ -31,11 +32,43 @@ public class MainActivity extends Activity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming","playlist-modify-private"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
 
+
+
+    //callback to onCreate
+    //extracts auth token and pass it into a config object
+    //passes config object into player object
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
     }
 
     @Override
@@ -63,9 +96,14 @@ public class MainActivity extends Activity implements
         }
     }
 
+    //callback to auth process
+    //onCreat -> onActivityResult -> onLoggedIn
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
+
+        //player example
+        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
     }
 
     @Override
