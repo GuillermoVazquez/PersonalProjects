c1b43ce9ac5036f9803cf8d400124f3fa826672e
GuillermoVazquez <wvvazque@gmail.com>
2018-08-19 17:05:16 -0400

    login/startup pipeline

--- songQ/app/src/main/java/vazquez/guillermo/songq/Fragments/JoinFragmnet.java
+++ songQ/app/src/main/java/vazquez/guillermo/songq/Fragments/JoinFragmnet.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import vazquez.guillermo.songq.R;
 
@@ -14,6 +15,8 @@ import vazquez.guillermo.songq.R;
  */
 public class JoinFragmnet extends Fragment {
 
+    Button button;
+    View v;
 
     public JoinFragmnet() {
         // Required empty public constructor
@@ -24,7 +27,18 @@ public class JoinFragmnet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_join_fragmnet, container, false);
+        v = inflater.inflate(R.layout.fragment_join_fragmnet, container, false);
+        button = v.findViewById(R.id.joinParty);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
+
     }
 
 }
--- songQ/app/src/main/java/vazquez/guillermo/songq/Fragments/LoginFragment.java
+++ songQ/app/src/main/java/vazquez/guillermo/songq/Fragments/LoginFragment.java
@@ -17,9 +17,8 @@ import vazquez.guillermo.songq.R;
  */
 public class LoginFragment extends Fragment {
 
-
-
-
+    Button button;
+    View v;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -30,7 +29,18 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        v = inflater.inflate(R.layout.fragment_login, container, false);
+        button = v.findViewById(R.id.startParty);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
 
     }
 
